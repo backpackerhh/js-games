be6c806a0898768676d5eb3ab20426dab4fd1c28
David Montesdeoca <backpackerhh@gmail.com>
2020-05-17 11:35:27 +0200

    Allow to create board

---
+++ memory-game/app.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const images = [
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+  ];
+
+  const grid = document.querySelector(".grid");
+
+  const createBoard = () => {
+    for (let idx = 0; idx < images.length; idx++) {
+      let card = document.createElement("img");
+      card.setAttribute("src", "images/blank.png");
+      card.setAttribute("data-id", idx);
+
+      grid.appendChild(card);
+    }
+  };
+
+  createBoard();
+});
