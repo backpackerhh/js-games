99b57a556f250d574e7d555afe52b2906e6cb027
David Montesdeoca <backpackerhh@gmail.com>
2020-05-17 11:55:21 +0200

    Allow to check for matches

--- memory-game/app.js
+++ memory-game/app.js
@@ -51,8 +51,10 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
 
   const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
   let cardsChosen = [];
   let cardsChosenIds = [];
+  let cardsWon = [];
 
   const createBoard = () => {
     for (let idx = 0; idx < images.length; idx++) {
@@ -79,7 +81,40 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const checkForMatch = () => {
-    alert("checkForMatch");
+    let cards = document.querySelectorAll("img");
+    const [optionOneId, optionTwoId] = cardsChosenIds;
+    const [optionOne, optionTwo] = cardsChosen;
+
+    if (optionOneId === optionTwoId) {
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+
+      alert("You have clicked the same image!");
+    } else if (optionOne === optionTwo) {
+      cards[optionOneId].setAttribute("src", "images/white.png");
+      cards[optionOneId].removeEventListener("click", flipCard);
+
+      cards[optionTwoId].setAttribute("src", "images/white.png");
+      cards[optionTwoId].removeEventListener("click", flipCard);
+
+      cardsWon.push(cardsChosen);
+
+      alert("You found a match");
+    } else {
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+
+      alert("Sorry, try again");
+    }
+
+    cardsChosen = [];
+    cardsChosenIds = [];
+
+    resultDisplay.textContent = cardsWon.length;
+
+    if (cardsWon.length === images.length / 2) {
+      resultDisplay.textContent = "Congratulations! You found them all!";
+    }
   };
 
   createBoard();
